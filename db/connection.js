473f1f5f5cd3ee784914f78ce57a0e87d1b7470a
CJ R <hello@cjr.co.de>
2016-12-16 10:38:33 -0700

    :rocket: Add database deploy info

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,3 @@
-const environment = 'development';
+const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile')[environment];
 module.exports = require('knex')(config);
